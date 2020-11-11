9d0e1d8b64fe1a71755ae7d56825817247075970
PapeMor2222 <tpsecures@gmail.com>
2020-11-11 19:32:27 +0000

    strapi du qrcode

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9286e3289fe1be113e565035d5a34fec'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '12d524cb-95f0-41d5-9613-ade9dcb1bbf2'
+};
